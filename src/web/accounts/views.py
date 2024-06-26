def4bd50761bb7c56528152cbeb99d098ffc9534
Zarar Anwar <zararanwar1234321@gmail.com>
2024-06-26 23:21:18 +0500

    UPDATES

--- src/web/accounts/views.py
+++ src/web/accounts/views.py
@@ -20,8 +20,8 @@ class LogoutView(View):
 class CrossAuthView(View):
     def get(self, request):
         if request.user.is_authenticated:
-            if request.user.is_staff or request.user.is_superuser:
-                return redirect("/admin/")
+            if request.user.is_staff and request.user.is_superuser:
+                return redirect("/admins/")
 
             if request.user.is_staff:
                 return redirect('/admins/')
